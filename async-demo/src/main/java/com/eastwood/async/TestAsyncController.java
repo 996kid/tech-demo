70b93e6508d41b74015e8aa67a7dc3254d716367
996kid <1069554723@qq.com>
2020-01-20 14:09:42 +0800

    lol

--- async-demo/src/main/java/com/eastwood/async/TestAsyncController.java
+++ async-demo/src/main/java/com/eastwood/async/TestAsyncController.java
@@ -1,8 +1,6 @@
 package com.eastwood.async;
 
-import com.eastwood.async.service.AsyncService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +37,10 @@ public class TestAsyncController extends BaseController{
     @GetMapping("/test")
     public void testExtendsField() {
         asyncService.helloWorld();
+    }
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "hello-world";
     }
 }
