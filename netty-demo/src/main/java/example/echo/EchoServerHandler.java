b1056720b1e2142637c8a0058b9b75a4759ecedc
996kid <996kid@gmail.com>
2020-12-18 11:25:51 +0800

    demo

---
+++ netty-demo/src/main/java/example/echo/EchoServerHandler.java
@@ -0,0 +1,35 @@
+package example.echo;
+
+/**
+ * @author 996kid
+ * @Description EchoServerHandler
+ * @Date 2020/12/17 17:11
+ */
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handler implementation for the echo server.
+ */
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
--- reactor-demo/src/main/java/io/projectreactor/ReactorDemo.java
+++ reactor-demo/src/main/java/io/projectreactor/ReactorDemo.java
@@ -1,9 +1,13 @@
 package io.projectreactor;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author yyh
@@ -13,10 +17,81 @@ import java.time.temporal.ChronoUnit;
 public class ReactorDemo {
 
     public static void main(String[] args) {
-        Flux.just("Hello", "World").subscribe(System.out::println);
-        Flux.fromArray(new Integer[] {1, 2, 3}).subscribe(System.out::println);
-        Flux.empty().subscribe(System.out::println);
-        Flux.range(1, 10).subscribe(System.out::println);
-        Flux.interval(Duration.of(10, ChronoUnit.SECONDS)).subscribe(System.out::println);
+//        Flux.just("Hello", "World").subscribe(System.out::println);
+//        Flux.fromArray(new Integer[] {1, 2, 3}).subscribe(System.out::println);
+//        Flux.empty().subscribe(System.out::println);
+//        Flux.range(1, 10).subscribe(System.out::println);
+//        Flux.interval(Duration.of(10, ChronoUnit.SECONDS)).subscribe(System.out::println);
+
+        List<Integer> elements = new ArrayList<>();
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .subscribe(elements::add);
+        System.out.println(elements);
+
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        s.request(Long.MAX_VALUE);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        elements.add(integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {}
+
+                    @Override
+                    public void onComplete() {}
+                });
+
+        //reactor is a push model. jdk1.8 streams is a pull model.
+
+        //backpressure
+        //Backpressure is when a downstream can tell an upstream to send it fewer data in order to prevent it from being overwhelmed.
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .subscribe(new Subscriber<Integer>() {
+                    private Subscription s;
+                    int onNextAmount;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        this.s = s;
+                        s.request(2);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        elements.add(integer);
+                        onNextAmount++;
+                        if (onNextAmount % 2 == 0) {
+                            s.request(2);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {}
+
+                    @Override
+                    public void onComplete() {}
+                });
+        //操作stream中的元素
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .map(i -> i * 2)
+                .subscribe(elements::add);
+
+        //  融合多个streams
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .map(i -> i * 2)
+                .zipWith(Flux.range(0, Integer.MAX_VALUE),
+                        (one, two) -> String.format("First Flux: %d, Second Flux: %d", one, two))
+                .subscribe(System.out::println);
     }
 }
