605ab10d480c1a78102df9cbefa251283f86c620
996kid <996kid@gmail.com>
2024-02-21 17:51:08 +0800

    2024-02-21

--- basic-demo/src/main/java/leetcode/array/FindRestaurant.java
+++ basic-demo/src/main/java/leetcode/array/FindRestaurant.java
@@ -67,4 +67,44 @@ public class FindRestaurant {
         }
         return map.get(minIndex).toArray(new String[0]);
     }
+
+
+    /**
+     * 使用一个哈希表记录 list1中每个餐厅对应的索引下标，然后遍历 list2
+     *  ，如果 list2
+     *   中的餐厅存在于哈希表中，那么说明该餐厅是两人共同喜爱的，计算它的索引和。如果该索引和比最小索引和小，则清空结果，将该餐厅加入结果中，
+     *   该索引和作为最小索引和；如果该索引和等于最小索引和，则直接将该餐厅加入结果中。
+     *
+     * list1 =
+     * ["Shogun","Tapioca Express","Burger King","KFC"]
+     * list2 =
+     * ["KFC","Shogun","Burger King"]
+     * 输出
+     * ["Burger King"]
+     * 预期结果
+     * ["Shogun"]
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public String[] findRestaurant1(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+        int minIndex = list1.length + list2.length - 2;
+        List<String> r = new ArrayList<>();
+        for (int i = 0; i < list2.length; i++) {
+            if (map.containsKey(list2[i])) {
+                if (i + map.get(list2[i]) < minIndex) {
+                    r.clear();
+                    r.add(list2[i]);
+                    minIndex = i + map.get(list2[i]);
+                } else if (i + map.get(list2[i]) == minIndex) {
+                    r.add(list2[i]);
+                }
+            }
+        }
+        return r.toArray(new String[0]);
+    }
 }
