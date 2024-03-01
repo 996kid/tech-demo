c14fda7db2b450302ce8ac520512f7e515c51c6f
996kid <996kid@gmail.com>
2024-03-01 18:02:43 +0800

    2024-02-29

---
+++ basic-demo/src/main/java/leetcode/array/MaximumProduct.java
@@ -0,0 +1,39 @@
+package leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * @author 996kid@gmail.com
+ * @date 2024/3/1
+ */
+public class MaximumProduct {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * 给你一个整型数组 nums ，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [1,2,3]
+     * 输出：6
+     * 示例 2：
+     *
+     * 输入：nums = [1,2,3,4]
+     * 输出：24
+     * 示例 3：
+     *
+     * 输入：nums = [-1,-2,-3]
+     * 输出：-6
+     * @param nums
+     * @return
+     */
+    public static int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        
+        return 0;
+    }
+}
